9a536dd9d661a6c717ca4bbef3a95a4b7648a82b
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-05-23 21:34:02 +0700

    chore: set up project

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    '@vueuse/nuxt',
+    '@nuxt/image',
+    '@pinia/nuxt'
+  ]
+})
