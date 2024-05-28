3e86242efa3017ede468c4caa73a4b6b1398d90b
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-05-28 12:44:44 +0700

    feat: add component ProductCart from flowbite

---
+++ components/ProductCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+<template>
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex flex-col items-center bg-[#F5F5F7]">
+            <img class="rounded-t-lg size-40" src="../public/favicon.ico" alt="" />
+        </div>
+
+        <div class="p-5">
+            <div class="flex flex-row items-center justify-between">
+                <h5 class="w-1/2 mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                    Mie Gelas
+                </h5>
+                <h5 class="w-1/2 mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                    Rp.1000
+                </h5>
+            </div>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.</p>
+            <a href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+            </a>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,20 @@ const { data } = await useFetch('/api/product')
 </script>
 
 <template>
-    <pre>{{ data }}</pre>
+    <!-- <pre>{{ data }}</pre> -->
+    <ul>
+        <li v-for="({
+            id,
+            logo,
+            title,
+            deskripsi,
+            price,
+            stok,
+            checkout,
+            status,
+        }) in data" :key="id">
+            {{ title }}
+        </li>
+    </ul>
+    <ProductCard></ProductCard>
 </template>
