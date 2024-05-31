a0f39e8188e0344c4f8d73948937895f218f8f2d
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-06-01 03:31:13 +0700

    feat: add go back in breadcrumb

--- components/breadcrumb/index.vue
+++ components/breadcrumb/index.vue
@@ -5,10 +5,15 @@ defineProps({
         required: true
     }
 })
+const route = useRouter()
 </script>
 
 <template>
     <header class="text-2xl text-[#3A3C40] dark:text-white font-semibold mb-7">
-        {{ title }}
+        <BaseButton @actions="route.back()"
+            class="cursor-pointer transition-all hover:duration-1000 hover:underline text-2xl text-[#3A3C40] dark:text-white font-semibold ">
+            Home
+        </BaseButton>
+        / {{ title }}
     </header>
 </template>
